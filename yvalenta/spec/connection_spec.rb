4973b32d146c8a1e7c82dd278aa858e3b9f12377
Yonatan <yvalenta@gmail.com>
2015-04-30 10:15:39 -0500

    Fix circle.yml and Hound recommendations

--- yvalenta/spec/connection_spec.rb
+++ yvalenta/spec/connection_spec.rb
@@ -5,7 +5,6 @@ describe 'Connection' do
   let(:regex) { /[0-9a-f]{ 8 }-[0-9a-f]{ 4 }-[0-9a-f]{ 4 }-[0-9a-f]{ 4 }-[0-9a-f]{ 12 }/}
 
   describe '#add' do
-
     it 'must return an uuid' do
       id = connection.add(5)
       expect(id).to match(regex)
@@ -19,7 +18,6 @@ describe 'Connection' do
   end
 
   describe '#find' do
-
     it 'must find a connection in a collection of connections by id' do
       id = connection.add(5)
       expect(connection.find(id)).to be == 5
@@ -33,7 +31,6 @@ describe 'Connection' do
   end
 
   describe '#destroy' do
-
     it 'must delete a connection from the collection' do
       id = connection.add(5)
       previous_count = connection.all.count
@@ -50,7 +47,6 @@ describe 'Connection' do
   end
 
   describe '#all' do
-
     it 'must return a hash with the connections' do
       id1 = connection.add(5)
       id2 = connection.add(10)
@@ -60,7 +56,6 @@ describe 'Connection' do
   end
 
   describe '#all_except' do
-
     it 'must return a hash with the connections with different id' do
       id_to_remove = connection.add(5)
       connection.add(10)
@@ -76,7 +71,6 @@ describe 'Connection' do
   end
 
   describe '#generate_key' do
-
     it 'must return a UUID key' do
       expect(connection.generate_key).to  match(regex)
     end
