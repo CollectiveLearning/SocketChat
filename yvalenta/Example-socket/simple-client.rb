b19b76f1fd0f26d40ad0d42ac5ab635dd5bd3951
Yonatan <yvalenta@gmail.com>
2015-04-30 09:30:29 -0500

    Recomendaciones hound

--- yvalenta/Example-socket/simple-client.rb
+++ yvalenta/Example-socket/simple-client.rb
@@ -1,12 +1,12 @@
-require 'socket'      # Sockets are in standard library
+require "socket" # Sockets are in standard library
 
-hostname = '127.0.0.1'
+hostname = "127.0.0.1"
 port = 2000
 
 s = TCPSocket.open(hostname, port)
 
 p "Sepase saber que entre al servidor: " + "#{(Socket.gethostname)}"
-while line = s.gets   # Read lines from the socket
-  puts line.chop      # And print with platform line terminator
+while line = s.gets # Read lines from the socket
+  puts line.chop # And print with platform line terminator
 end
-s.close               # Close the socket when done
+s.close # Close the socket when done
--- yvalenta/Example-socket/simple-server.rb
+++ yvalenta/Example-socket/simple-server.rb
@@ -1,12 +1,12 @@
-require 'socket'               # Get sockets from stdlib
+require "socket" # Get sockets from stdlib
 
 server = TCPServer.open(2000)  # Socket to listen on port 2000
 contador = 0
-loop {                         # Servers run forever
-  client = server.accept       # Wait for a client to connect
-  client.puts(Time.now.ctime)  # Send the time to the client
+loop { # Servers run forever
+  client = server.accept # Wait for a client to connect
+  client.puts(Time.now.ctime) # Send the time to the client
   client.puts "La conexion esta abierta en el puerto 2000 del servidor \ny se ve reflejada el mensaje en el socket abierto por el cliente"
   puts "Mensaje del servidor Notificando cuantas veces entraron \nHan entrado: #{contador += 1} ve(z)(ces)"
   client.puts "The server has closed the connection. Bye!"
-  client.close                 # Disconnect from the client
+  client.close # Disconnect from the client
 }
